4462e9b56a171b773a0b8e7c97b9e0faceec8103
akash12patel12 <115473880+akash12patel12@users.noreply.github.com>
2023-02-19 12:29:42 +0530

    Update expenseRoute.js

--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -8,4 +8,5 @@ router.post('/addex',userAuth.authenticate,  expenseController.addex);
 router.post('/getAll',userAuth.authenticate, expenseController.getAll );
 router.post('/delete',userAuth.authenticate, expenseController.deleteOne);
 router.get('/getTotalNumberOfExpenses', userAuth.authenticate, expenseController.getTotalNumberOfExpenses)
+router.get('/getTotalOfUser', userAuth.authenticate, expenseController.getTotalOfUser);
 module.exports = router;
